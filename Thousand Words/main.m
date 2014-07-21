51ffdc3f2d4fa581231ae81e7ecd0a86094e2116
Divakaran Jeyachandran <diva@divakarans-mbp.gateway.2wire.net>
2014-07-21 02:05:00 -0700

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Divakaran Jeyachandran on 7/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DJAppDelegate class]));
+    }
+}
